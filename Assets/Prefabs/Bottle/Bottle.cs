d7c43bf5b5240c93acd1ad313eb51d9e54075552
Jordan Hill <jordan.ri.hill@gmail.com>
2019-11-16 19:22:29 -0600

    Adds bottle pickup.

---
+++ Assets/Prefabs/Bottle/Bottle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bottle : MonoBehaviour {
+    public float rotationSpeed;
+
+    void Update() {
+        this.transform.Rotate(Vector3.up, Time.deltaTime * this.rotationSpeed);
+    }
+
+    public void OnTriggerEnter(Collider other) {
+        Destroy(this.gameObject);
+    }
+}
