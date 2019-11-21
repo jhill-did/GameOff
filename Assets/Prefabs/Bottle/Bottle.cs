401ed2e3675cf666f2d95aca9d03c32dbe05555a
Jordan Hill <jordan.ri.hill@gmail.com>
2019-11-20 21:35:14 -0600

    Adds Bottle prefab.

--- Assets/Prefabs/Bottle/Bottle.cs
+++ Assets/Prefabs/Bottle/Bottle.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class Bottle : MonoBehaviour {
     public float rotationSpeed;
+    public AudioClip pickUpSound;
 
     void Update() {
         this.transform.Rotate(Vector3.up, Time.deltaTime * this.rotationSpeed);
     }
 
     public void OnTriggerEnter(Collider other) {
+        AudioSource.PlayClipAtPoint(pickUpSound, this.transform.position);
         Destroy(this.gameObject);
     }
 }
