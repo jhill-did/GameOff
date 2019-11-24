fe69e1839c31464936bc557953bde1730455e48b
TheWholeRatio <thewholeratio@gmail.com>
2019-11-23 23:39:35 -0600

    modified floating rock. More rock parts. Pick up bottle interaction.

--- Assets/Props/Rocks/RockIndicator.cs
+++ Assets/Props/Rocks/RockIndicator.cs
@@ -4,20 +4,49 @@ using UnityEngine;
 
 public class RockIndicator : MonoBehaviour
 {
+    public PhysicMaterial rockPhysicsMaterial;
+    public Material rockMaterial;
+
+    public GameObject bottle;
+    public float rockPartMass;
+    bool isDestroyed = false;
+
+
     // Start is called before the first frame update
     void Start()
     {
         foreach (Transform child in transform)
         {
-            child.gameObject.GetComponent<MeshCollider>().convex = true;
-            //child is your child transform
-            child.gameObject.AddComponent<Rigidbody>();
+            child.gameObject.GetComponent<MeshRenderer>().material = rockMaterial;
         }
     }
 
+
+    public IEnumerator breakApart()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.GetComponent<MeshCollider>().convex = true;
+            child.gameObject.AddComponent<Rigidbody>();
+            child.gameObject.GetComponent<Rigidbody>().mass = rockPartMass;
+            child.gameObject.GetComponent<MeshRenderer>().material = rockMaterial;
+            child.gameObject.GetComponent<MeshCollider>().material = rockPhysicsMaterial;
+        }
+        yield return new WaitForSeconds(0.01f);
+        foreach (Transform child in transform)
+        {
+            child.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+        var floatingScript = GetComponent<FloatingRock>();
+        Destroy(floatingScript);
+    }
     // Update is called once per frame
     void Update()
     { 
-    
+        if(bottle == null && !isDestroyed)
+        {
+            StartCoroutine(breakApart());
+            isDestroyed = true;
+        }
     }
 }
