8ba1630ed6c3ec9d590bd903be6ca05649483ace
TheWholeRatio <thewholeratio@gmail.com>
2019-11-23 22:24:29 -0600

    Implemented destructible rock?

---
+++ Assets/Props/Rocks/RockIndicator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockIndicator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.GetComponent<MeshCollider>().convex = true;
+            //child is your child transform
+            child.gameObject.AddComponent<Rigidbody>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+    
+    }
+}
