e37f4c1398c5c038a496362035d333363640a3b3
Jordan Hill <jordan.ri.hill@gmail.com>
2019-11-30 16:50:18 -0600

    Adds splash sound component to character.

---
+++ Assets/Prefabs/Character/CharacterSplash.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSplash : MonoBehaviour {
+    public AudioClip splashSound;
+    public new Rigidbody rigidbody;
+    public float velocityThreshold;
+    public float heightThreshold;
+
+    private bool underWater = false;
+
+    void Update() {
+        var playerVelocity = rigidbody.velocity.magnitude;
+        bool fastEnough = playerVelocity > velocityThreshold;
+
+        var verticalPosition = transform.position.y;
+        var rightHeight = verticalPosition < this.heightThreshold;
+
+        if (fastEnough && rightHeight && !underWater) {
+            // Play a sound as we hit the water.
+            AudioSource.PlayClipAtPoint(splashSound, transform.position);
+
+            underWater = true;
+        }
+
+        // Make sure we don't trigger the sound again until we exit the water.
+        if (!rightHeight) {
+            this.underWater = false;
+        }
+    }
+}
