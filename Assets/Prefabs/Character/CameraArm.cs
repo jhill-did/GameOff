3ba8eb617a79ded0962f6ac4d70241230d74bc85
Jordan Hill <jordan.ri.hill@gmail.com>
2019-12-01 00:03:18 -0600

    Adds camera arm script to stop player camera from penetrating walls.

---
+++ Assets/Prefabs/Character/CameraArm.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraArm : MonoBehaviour {
+    public new GameObject camera;
+    private float maxDistance;
+
+    void Start() {
+        maxDistance = (transform.position - camera.transform.position).magnitude;
+    }
+
+    void Update() {
+        var start = transform.position;
+        var ray = new Ray(start, -transform.forward);
+        var hit = Physics.SphereCast(ray, 0.1f, out var hitInfo, maxDistance + 0.5f);
+
+        var distance = hit ? hitInfo.distance : maxDistance;
+
+        camera.transform.position = start + transform.forward * -distance;
+    }
+}
