e65fdd7a12eecc2fbf997514d51672840b4e8a98
Jordan Hill <jordan.ri.hill@gmail.com>
2019-11-30 18:34:05 -0600

    Adds crosshair animation.

---
+++ Assets/Prefabs/UI/Crosshair/CrosshairAnimation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrosshairAnimation : MonoBehaviour {
+    public Character player;
+    public Image crosshair;
+    public float fadeRate;
+    private float opacity;
+
+    void Update() {
+        opacity = player.chargingLaunch
+            ? Mathf.Clamp01(opacity + fadeRate * Time.deltaTime)
+            : Mathf.Clamp01(opacity - fadeRate * Time.deltaTime);
+
+        var charge = player.launchHoldTimer;
+
+        crosshair.material.SetFloat("Charge", charge);
+        crosshair.material.SetFloat("Opacity", opacity);
+    }
+}
